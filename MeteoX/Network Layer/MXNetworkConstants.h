2cf707bc01961530e11af8ef6012a4c9c18430d0
Pavlo Dumyak <pavlo.dumyak@inoxoft.com>
2016-05-31 23:47:36 +0300

    Setup network layer

---
+++ MeteoX/Network Layer/MXNetworkConstants.h
@@ -0,0 +1,20 @@
+//
+//  MXNetworkConstants.h
+//  MeteoX
+//
+//  Created by Pavlo Dumyak on 31.05.16.
+//  Copyright © 2016 PavloDumyak. All rights reserved.
+//
+
+#ifndef MXNetworkConstants_h
+#define MXNetworkConstants_h
+
+// REST API
+
+static NSString *const mBaseURL = @"http://api.openweathermap.org/";
+static NSString *const mAppID = @"&appid=a575ffe0b2c85312ed4186cbb0392aa8";
+
+static NSString *const mWeatherByCityNameURL = @"/data/2.5/weather?q=%@";
+static NSString *const mWeatheForAFiveDayURL =  @"data/2.5/forecast?q=%@,ua";
+
+#endif /* MXNetworkConstants_h */
---
+++ MeteoX/Network Layer/test/MXHttpRequestManager.h
@@ -0,0 +1,39 @@
+//
+//  MXHttpRequestManager.h
+//  MeteoX
+//
+//  Created by Pavlo Dumyak  on 31.05.16.
+//  Copyright © 2016 PavloDumyak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef NS_ENUM(NSUInteger, HttpRequestType) {
+    HttpRequestTypeGet,
+    HttpRequestTypePost,
+    HttpRequestTypePut,
+    HttpRequestTypeDelete
+};
+
+typedef void (^HttpRequestSuccessHandler)(NSURLSessionDataTask *task, id responseObject);
+typedef void (^HttpRequestFailHandler)(NSURLSessionDataTask *_Nullable task, NSError *error);
+
+@interface MXHttpRequestManager : NSObject
+
+@property (nonatomic, copy) NSString *link;
+@property (nonatomic, assign) HttpRequestType type;
+@property (nonatomic, strong) NSDictionary *params;
+@property (nonatomic, strong) NSString *tokenType;
+@property (nonatomic, copy, nullable) HttpRequestSuccessHandler httpRequestSuccessHandler;
+@property (nonatomic, copy, nullable) HttpRequestFailHandler httpRequestFailHandler;
+
+- (instancetype)initWithLink:(NSString *)link;
+- (instancetype)initWithLink:(NSString *)link ofType:(HttpRequestType)type withParams:(nullable NSDictionary *)params;
+
+- (void)sendRequest;
+
+@end
+
+NS_ASSUME_NONNULL_END
