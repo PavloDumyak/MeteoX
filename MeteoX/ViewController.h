ecd21c65b60794afcae78bdcb7b4f1907314ad17
Pavlo Dumyak <pavlo.dumyak@inoxoft.com>
2016-05-31 23:05:50 +0300

    Initial Commit

---
+++ MeteoX/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MeteoX
+//
+//  Created by Pavlo Dumyak on 31.05.16.
+//  Copyright © 2016 PavloDumyak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
